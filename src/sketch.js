dc8751e7263697edbbefe81f1c2ffb85b8f15237
B3P <39571509+Wikipediano@users.noreply.github.com>
2021-08-25 13:32:30 -0700

    Add FIsher-Yates shuffle algorithm

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,32 @@
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(400, 400);
+    let a = generatePattern(8);
+    // console.log(a);
 }
 
 function draw() {
-  background(220);
+    background(220);
+}
+
+function generatePattern(n) {
+    // declare array of length n
+    let a = [];
+
+    // initialize array values to be numbers in order
+    for (let i = 0; i < n; i++) {
+        a[i] = i + 1;
+    }
+
+    // shuffle the array to create a random pattern
+    // Fisher-Yates Suffle Algorithm
+    for (let i = n - 1; i > 0; i--) {
+        let j = int(random(0, i + 1));
+        // console.log(j);
+        let temp = a[j];
+        a[j] = a[i];
+        a[i] = temp;
+    }
+
+    //return the generated pattern
+    return a;
 }
